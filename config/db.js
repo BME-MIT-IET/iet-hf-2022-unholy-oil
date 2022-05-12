f56525916333d263db680de999f04bfa9580e3a7
triszt4n <trisztanpiller@gmail.com>
2022-05-12 15:36:57 +0200

    Apply prettier formatting on all codebase

--- config/db.js
+++ config/db.js
@@ -1,13 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const connectionString = '';
+const connectionString = 'mongodb://localhost:27017/cica'
 
-mongoose.connect(
-    connectionString, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-);
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
-
-module.exports = mongoose;
+module.exports = mongoose
