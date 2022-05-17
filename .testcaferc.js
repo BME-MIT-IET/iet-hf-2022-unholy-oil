d809b9727a8ea5ed02ae778d8686bb7988e29b30
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-05-17 17:56:27 +0200

    Add End to End tests to application (#24)
    * Add testcafe dependency
    
    * Add id to logout button, for easier e2e tests
    
    * configure testcafe
    
    * Add login test fixture
    
    * Create complex test cases
    
    * Add to storage testcase
    
    * Refactor create bunker test
    
    * refactor Add new food testcase
    
    * Refactor add to storage test case
    
    * Code cleanup
    
    * Add mongo action to test workflow
    
    * specify headless browser for tests
    
    * Re disable screenshots

---
+++ .testcaferc.js
@@ -0,0 +1,14 @@
+require('dotenv').config({ path: '.test.env' })
+
+module.exports = {
+  skipJsErrors: true,
+  hostname: 'localhost',
+  disableScreenshots: true,
+  quarantineMode: true,
+  src: './test/e2e/',
+  appCommand: 'DOTENV_CONFIG_PATH=.test.env node index.js',
+  appInitDelay: 3000,
+  userVariables: {
+    app_url: 'http://localhost:3000',
+  },
+}
---
+++ test/e2e/login.js
@@ -0,0 +1,26 @@
+import { Selector } from 'testcafe'
+import { pahtName } from './utils'
+
+fixture`Login test`.page`localhost:3000`
+
+test('Login and log out', async (t) => {
+  await t
+    .typeText('#username', process.env.ADMIN_USER)
+    .typeText('#password', process.env.ADMIN_PASSWORD)
+    .click('input[type=submit]')
+
+  const btnLogout = Selector('#btn-logout')
+  let path = await pahtName()
+
+  await t
+    .expect(path)
+    .eql('/bunkers', 'Login should redirect to bunkers page')
+    .expect(btnLogout.exists)
+    .ok('Logout button should exists, and be visible')
+
+  await t.click(btnLogout)
+
+  path = await pahtName()
+
+  await t.expect(path).eql('/', 'Logout should redirect to login page')
+})
