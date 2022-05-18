d695bdcb09d42da9dca8df8d5d3adf1a52eacff1
Trisztán Piller <60111772+triszt4n@users.noreply.github.com>
2022-05-19 00:01:28 +0200

    Dockerize and deploy (#27)
    * Add initial docker files
    
    * Fully solve dockerization #8
    
    * Fix up dockerization
    
    * Trying out deployment
    
    * Create empty .env file
    
    * Remove volume from docker-compose
    
    * Use heroku assigned port
    
    * Let heroku handle ports
    
    * Changing database access to localhost
    
    * Use internal docker container name for db connection
    
    * Catch error of mongoose
    
    * Custom localhost of heroku containers trial
    
    * Using authSource on mongo connect string
    
    * Use docker dns without port
    
    * Container name forced
    
    * Simplify docker settings
    
    * Migrate db to Mongo Atlas

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-const connectionString = process.env.MONGO_CONNECTION_URL || ''
+const connectionString =
+  process.env.MONGO_CONNECTION_URL ||
+  `mongodb://localhost:27017/bunkermgr?authSource=admin`
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
